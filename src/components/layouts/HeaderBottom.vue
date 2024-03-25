91dffb41f3a1fd6fb2a9d6aac7e8b65496674656
abdulsamed koçyiğit <abdulsamed.kcygt@gmail.com>
2024-03-25 17:11:44 +0300

    new header bottom

---
+++ src/components/layouts/HeaderBottom.vue
@@ -0,0 +1,328 @@
+<template>
+  <div class="header-bottom">
+    <div class="container-xx1">
+      <div class="header-bottom-inside">
+        <nav id="navigation">
+          <div class="category-level-1 position-relative">
+            <ul class="d-flex justify-content-center p-0 mb-0">
+              <li class="has-sub-category" data-selector="first-level-navigation">
+                <a href="/kategori/led-ampuller" class="text-center">
+                  <span class>Led Ampuller</span>
+                </a>
+                <div class="sub-category category-level-2">
+                  <ul>
+                    <li class="navigation-category-image">
+                      <a href="/kategori/led-ampuller-e14">
+                        <div
+                          class="rounded"
+                          style="
+                            /* background-image: url(//ideacdn.net/shop/ad/07/myassets/categories/1342/kugu.jpg?revision=1699945978); */
+                            background-size: contain;
+                            background-position: center;
+                            background-repeat: no-repeat;
+                          "
+                        ></div>
+                        <span>Led Ampuller E14</span>
+                      </a>
+                    </li>
+                    <li class="navigation-category-image">
+                      <a href="/kategori/led-ampuller-e27">
+                        == $0
+                        <div
+                          class="rounded"
+                          style="
+                            /* background-image: url(//ideacdn.net/shop/ad/07/myassets/categories/1343/9535429345330.jpg?revision=1699945936); */
+                            background-size: contain;
+                            background-position: center;
+                            background-repeat: no-repeat;
+                          "
+                        ></div>
+                        <span>Led Ampuller E27</span>
+                      </a>
+                    </li>
+                    <li class="navigation-category-image">
+                      <a href="/kategori/led-flamentli-ve-rustik-ampuller">
+                        == $0
+                        <div
+                          class="rounded"
+                          style="
+                            /* background-image: url(//ideacdn.net/shop/ad/07/myassets/categories/1343/9535429345330.jpg?revision=1699945936); */
+                            background-size: contain;
+                            background-position: center;
+                            background-repeat: no-repeat;
+                          "
+                        ></div>
+                        <span>Led Flamentli ve Rustik Ampuller E27</span>
+                      </a>
+                    </li>
+                    <li class="navigation-category-image">
+                      <a href="/kategori/led-gece-ampulleri">
+                        == $0
+                        <div
+                          class="rounded"
+                          style="
+                            /* background-image: url(//ideacdn.net/shop/ad/07/myassets/categories/1343/9535429345330.jpg?revision=1699945936); */
+                            background-size: contain;
+                            background-position: center;
+                            background-repeat: no-repeat;
+                          "
+                        ></div>
+                        <span>Led Gece Ampulleri</span>
+                      </a>
+                    </li>
+                    <li class="navigation-category-image">
+                      <a href="/kategori/led-spot-ampuller">
+                        == $0
+                        <div
+                          class="rounded"
+                          style="
+                            /* background-image: url(//ideacdn.net/shop/ad/07/myassets/categories/1343/9535429345330.jpg?revision=1699945936); */
+                            background-size: contain;
+                            background-position: center;
+                            background-repeat: no-repeat;
+                          "
+                        ></div>
+                        <span>Led Spot Ampuller</span>
+                      </a>
+                    </li>
+                    <li class="navigation-category-image">
+                      <a href="/kategori/led-kapsul-ampuller">
+                        == $0
+                        <div
+                          class="rounded"
+                          style="
+                            /* background-image: url(//ideacdn.net/shop/ad/07/myassets/categories/1343/9535429345330.jpg?revision=1699945936); */
+                            background-size: contain;
+                            background-position: center;
+                            background-repeat: no-repeat;
+                          "
+                        ></div>
+                        <span>Led Kapsul Ampuller</span>
+                      </a>
+                    </li>
+                    <li class="navigation-category-image">
+                      <a href="/kategori/led-ralina-ampuller">
+                        == $0
+                        <div
+                          class="rounded"
+                          style="
+                            /* background-image: url(//ideacdn.net/shop/ad/07/myassets/categories/1343/9535429345330.jpg?revision=1699945936); */
+                            background-size: contain;
+                            background-position: center;
+                            background-repeat: no-repeat;
+                          "
+                        ></div>
+                        <span>Led Ralina Ampuller</span>
+                      </a>
+                    </li>
+                    <li class="navigation-category-image">
+                      <a href="/kategori/led-yuksek-lumen-ampuller">
+                        == $0
+                        <div
+                          class="rounded"
+                          style="
+                            /* background-image: url(//ideacdn.net/shop/ad/07/myassets/categories/1343/9535429345330.jpg?revision=1699945936); */
+                            background-size: contain;
+                            background-position: center;
+                            background-repeat: no-repeat;
+                          "
+                        ></div>
+                        <span>Led Yuksek Lümen Ampuller</span>
+                      </a>
+                    </li>
+                    <li class="navigation-category-image">
+                      <a href="/kategori/led-t-5-floresanlar">
+                        == $0
+                        <div
+                          class="rounded"
+                          style="
+                            /* background-image: url(//ideacdn.net/shop/ad/07/myassets/categories/1343/9535429345330.jpg?revision=1699945936); */
+                            background-size: contain;
+                            background-position: center;
+                            background-repeat: no-repeat;
+                          "
+                        ></div>
+                        <span>Led T5 Floresanlar</span>
+                      </a>
+                    </li>
+                    <li class="navigation-category-image">
+                      <a href="/kategori/led-t-8-floresanlar">
+                        == $0
+                        <div
+                          class="rounded"
+                          style="
+                            /* background-image: url(//ideacdn.net/shop/ad/07/myassets/categories/1343/9535429345330.jpg?revision=1699945936); */
+                            background-size: contain;
+                            background-position: center;
+                            background-repeat: no-repeat;
+                          "
+                        ></div>
+                        <span>Led T8 Floresanlar</span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li class="has-sub-category" data-selector="first-level-navigation">
+                <a href="/kategori/ic-mekan-led-armaturler" class="text-center">
+                  <span class>Ic Mekan Led Armatürler</span>
+                </a>
+                <div class="sub-category category-level-2">
+                  <ul>
+                    <li class="navigation-category-image">
+                      <a href="/kategori/led-downlight-armaturler">
+                        <div
+                          class="rounded"
+                          style="
+                            background-image: url(//ideacdn.net/shop/ad/07/myassets/categories/1369/meson.jpg?revision=1699947971);
+                            background-size: contain;
+                            background-position: center;
+                            background-repeat: no-repeat;
+                          "
+                        ></div>
+                        <span>Led Downlight Armatürler</span>
+                      </a>
+                    </li>
+                    <li class="navigation-category-image">
+                      <a href="/kategori/ic-led-tavan-ve-duvar-armaturleri">
+                        <div
+                          class="rounded"
+                          style="
+                            background-image: url(//ideacdn.net/shop/ad/07/myassets/categories/3114/index.jpg?revision=1709808168);
+                            background-size: contain;
+                            background-position: center;
+                            background-repeat: no-repeat;
+                          "
+                        ></div>
+                        <span>Led Tavan ve Duvar Armatürleri</span>
+                      </a>
+                    </li>
+                    <li class="navigation-category-image">
+                      <a href="/kategori/led-spot-armaturleri">
+                        <div
+                          class="rounded"
+                          style="
+                            background-image: url(//ideacdn.net/shop/ad/07/myassets/categories/1375/goldx-kristal-spot-armatur-ze631-7d0e.jpg?revision=1699947576);
+                            background-size: contain;
+                            background-position: center;
+                            background-repeat: no-repeat;
+                          "
+                        ></div>
+                        <span>Led Spot Armatürleri</span>
+                      </a>
+                    </li>
+                    <li class="navigation-category-image">
+                      <a href="/kategori/led-lineer-bant-tipi-armaturler">
+                        <div
+                          class="rounded"
+                          style="
+                            background-image: url(//ideacdn.net/shop/ad/07/myassets/categories/1372/trunk.jpg?revision=1699947804);
+                            background-size: contain;
+                            background-position: center;
+                            background-repeat: no-repeat;
+                          "
+                        ></div>
+                        <span>Led Lineer Bant Tipi Armatürler</span>
+                      </a>
+                    </li>
+                    <li class="navigation-category-image">
+                      <a href=" /kategori/led-panel-armaturler">
+                        <div
+                          class="rounded"
+                          style="
+                            background-image: url(//ideacdn.net/shop/ad/07/myassets/categories/1374/60x60.jpg?revision=1699947653);
+                            background-size: contain;
+                            background-position: center;
+                            background-repeat: no-repeat;
+                          "
+                        ></div>
+                        <span>Led Panel Armatürler</span>
+                      </a>
+                    </li>
+                    <li class="navigation-category-image">
+                      <a href="/kategori/led-ofis-magaza-ve-vitrin-aydinlatma-armaturleri">
+                        <div
+                          class="rounded"
+                          style="
+                            background-image: url(//ideacdn.net/shop/ad/07/myassets/categories/1373/magaza.jpg?revision=1699947706);
+                            background-size: contain;
+                            background-position: center;
+                            background-repeat: no-repeat;
+                          "
+                        ></div>
+                        <span>Led Ofis-Magaza ve Vitrin Aydinlatma Armatürleri</span>
+                      </a>
+                    </li>
+                    <li class="navigation-category-image">
+                      <a href=" /kategori/led-etanj-garaj-otopark-armaturleri">
+                        == $0
+                        <div
+                          class="rounded"
+                          style="
+                            background-image: url(//ideacdn.net/shop/ad/07/myassets/categories/1370/etanj.jpg?revision=1699947908);
+                            background-size: contain;
+                            background-position: center;
+                            background-repeat: no-repeat;
+                          "
+                        ></div>
+                        <span>Led Etanj Garaj Otopark Armatürleri</span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li class="has-sub-category" data-selector="firs-level-navigation"></li>
+              <li class="has-sub-category" data-selector="firs-level-navigation"></li>
+              <li class="has-sub-category" data-selector="firs-level-navigation"></li>
+              <li class="has-sub-category" data-selector="firs-level-navigation"></li>
+              <li class="has-sub-category" data-selector="firs-level-navigation"></li>
+              <li class="has-sub-category" data-selector="firs-level-navigation"></li>
+              <li class="has-sub-category" data-selector="firs-level-navigation"></li>
+              <li class="has-sub-category" data-selector="firs-level-navigation"></li>
+              <li class="has-sub-category" data-selector="firs-level-navigation"></li>
+              <li class="has-sub-category" data-selector="firs-level-navigation"></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderBottom',
+}
+</script>
+
+<style scoped>
+.has-sub-category {
+  position: relative; /* Alt menüyü konumlandırmak için */
+  display: inline-block; /* Yan yana yerleştirmek için */
+}
+
+.sub-category {
+  display: none; /* Başlangıçta alt menüyü gizle */
+  position: absolute;
+  top: 100%; /* Ana menüden aşağıya konumlandır */
+  left: 0;
+  background-color: #f9f9f9;
+  min-width: 200px;
+}
+
+.has-sub-category:hover .sub-category {
+  display: block; /* Fare üzerine geldiğinde alt menüyü göster */
+}
+
+/* Yan yana sıralamak ve ortalamak için */
+.category-level-1 {
+  display: flex;
+  justify-content: center;
+  list-style-type: none;
+  padding: 0;
+}
+
+.category-level-1 li {
+  margin: 0 10px; /* Öğeler arasındaki boşluğu ayarlayabilirsiniz */
+}
+</style>
