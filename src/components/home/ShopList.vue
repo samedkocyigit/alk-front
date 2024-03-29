ca7f64c55f4907600e96d3a6b85bcca40e0387be
abdulsamed koçyiğit <abdulsamed.kcygt@gmail.com>
2024-03-29 09:57:23 +0300

    The swiper has been adden on brandCard

--- src/components/home/ShopList.vue
+++ src/components/home/ShopList.vue
@@ -7,27 +7,37 @@
       </h4>
       <h4 class="text-third-100">See All</h4>
     </div>
-    <div class="w-full flex max-md:grid max-md:grid-cols-2 gap-5 max-md:gap-3 mt-5 flex-wrap">
-      <RouterLink
-        v-for="item in brands.data"
-        :key="item._id"
-        class="h-[220px] shadow-d-30 block bg-white rounded-md w-[183px] max-md:w-full"
-        to="/"
+    <div class="w-full flex p-4 gap-4 relative">
+      <swiper
+        ref="mySwiper"
+        style="padding: 0 8px 10px 8px"
+        class="w-full pb-2"
+        :slide-prev-view="'auto'"
+        :space-between="10"
+        :loop="brands.data.length >= 2"
+        :autoplay="{ delay: 3000, disableOnInteraction: false }"
+        @swiper="onSwiper"
       >
-        <div class="w-full h-[140px] flex justify-center items-center bg-primary-400">
-          <img class="w-[70%] object-contain" :src="item.photo[0]" alt="" />
-        </div>
-        <div class="flex py-3 flex-col items-center">
-          <p class="font-medium">{{ item.name }}</p>
-          <p class="font-base text-gray-400"><i class="ri-shield-check-fill text-green-500 mr-1"></i>Official store</p>
-        </div>
-      </RouterLink>
+        <swiper-slide v-for="brand in brands.data" :key="brand.id" style="width: auto" class="w-fit">
+          <RouterLink :to="`/brands/${brand.id}`">
+            <BrandCard :brand="brand" />
+          </RouterLink>
+        </swiper-slide>
+      </swiper>
     </div>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import 'swiper/css'
+
+const mySwiper = ref(null)
+
+const onSwiper = (swiper) => {
+  mySwiper.value = swiper
+}
 
 const brands = ref([])
 const loading = ref(true)
@@ -35,7 +45,7 @@ const loading = ref(true)
 async function fetchBrands() {
   if (brands.value.length === 0) {
     try {
-      const response = await fetch('/api/brands')
+      const response = await fetch('/brands')
       if (!response.ok) {
         throw new Error('Fetch işlemi başarısız')
       }
@@ -44,7 +54,6 @@ async function fetchBrands() {
       brands.value = data.data
       console.log('response=>', brands.value)
       console.log('foti', brands.value.data[0].photo[0])
-
       loading.value = false
     } catch (error) {
       console.log('hata:', error.message)
@@ -52,6 +61,8 @@ async function fetchBrands() {
     }
   }
 }
+
+import BrandCard from './BrandCard.vue'
 
 fetchBrands()
 </script>
