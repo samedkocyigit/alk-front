f59211cbca8745bfc7336f23efea680e8829f606
abdulsamed koçyiğit <abdulsamed.kcygt@gmail.com>
2024-04-05 14:25:47 +0300

    authStore has been defined for authentication

--- src/stores/auth.store.js
+++ src/stores/auth.store.js
@@ -1,57 +1,56 @@
 /* eslint-disable no-unused-vars */
+// import { defineStore } from 'pinia'
+// auth.store.js
+
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
-export const AuthStore = defineStore('auth', () => {
-  const state = {
-    user: null,
-    isLoggedIn: false,
-  }
-
-  const setUser = (userData) => {
-    state.user = userData
-  }
-
-  return {
-    state,
-    actions: {
-      setUser,
-    },
-  }
+export const AuthStore = ref({
+  user: null,
+  isLoggedIn: false,
 })
-export const startAuthStore = async (res) => {
+
+export const loginAuthStore = async (userData) => {
+  const authStore = AuthStore.value
+  authStore.user = userData
+  authStore.isLoggedIn = true
+}
+export const logoutAuthStore = async () => {
+  const authStore = AuthStore.value
+  localStorage.removeItem('access_token')
+  authStore.user = null
+  authStore.isLoggedIn = false
+}
+
+export const startAuthStore = async () => {
+  const authStore = AuthStore.value
   if (localStorage.getItem('access_token')) {
+    // Eğer access_token varsa, kullanıcı oturumu açıktır
     try {
-      const data = res
-      AuthStore.setUser(data)
-      AuthStore.state.isLoggedIn = true
-      console.log('initAuthStore', AuthStore.state)
+      // localStorage'den kullanıcı bilgilerini al
+      const userData = localStorage.getItem('user')
+      if (userData !== null) {
+        authStore.user = JSON.parse(userData)
+        authStore.isLoggedIn = true
+      } else {
+        // Kullanıcı bilgileri localStorage'da bulunamadı
+        console.warn('User data not found in localStorage')
+        // Gerekli işlemleri yap
+        authStore.user = null
+        authStore.isLoggedIn = false
+        // window.location.reload() gibi bir işlem de yapılabilir
+      }
     } catch (error) {
       console.error('Error fetching user information:', error)
+      // Hata durumunda gerekli işlemleri yap
       localStorage.removeItem('access_token')
-      AuthStore.state.user = null
-      AuthStore.state.isLoggedIn = false
+      authStore.user = null
+      authStore.isLoggedIn = false
       window.location.reload()
     }
   } else {
-    AuthStore.state.user = null
-    AuthStore.state.isLoggedIn = false
-    return // Oturum kapalı olduğunda fonksiyonu sonlandır
+    // Eğer access_token yoksa, kullanıcı oturumu kapalıdır
+    authStore.user = null
+    authStore.isLoggedIn = false
   }
 }
-
-// refactor user store
-
-// export const useAuthStore = defineStore('auth', () => {
-//   const state = ref({
-//     user: null,
-//     isLoggedIn: false,
-//   })
-
-//   const setAuthStore = (data) => {
-//     state.value = data
-//   }
-//   return {
-//     state,
-//     setAuthStore,
-//   }
-// })
