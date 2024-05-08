e51e048eccff4a1b977ce03d9380f7f2b9c30170
abdulsamed koçyiğit <abdulsamed.kcygt@gmail.com>
2024-05-08 17:42:28 +0300

    some tyy with swiper

--- src/components/home/ShopList.vue
+++ src/components/home/ShopList.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onBeforeMount, nextTick } from 'vue'
 import { Swiper, SwiperSlide } from 'swiper/vue'
 import 'swiper/css'
 
@@ -11,56 +11,43 @@ const brands = ref([])
 const loading = ref(true)
 
 async function fetchBrands() {
-  if (brands.value.length === 0) {
-    try {
-      const res = await getBrandsApi()
-      // const data = res.data
-      // if (!data || !data.data) throw new Error('Gelen veriler istenilen formatta değil')
-      brands.value = res.data.data
-      loading.value = false
-    } catch (error) {
-      console.log('hata:', error.message)
-      loading.value = false
-    }
+  try {
+    const res = await getBrandsApi()
+    brands.value = res.data.data
+    loading.value = false
+  } catch (error) {
+    console.log('hata:', error.message)
+    loading.value = false
   }
 }
 
 const onSwiper = (swiper) => {
   mySwiper.value = swiper
-  console.log('Brands Swiper:', swiper)
+  console.log('mySwiper', mySwiper.value)
 }
 
-fetchBrands()
+onBeforeMount(async () => {
+  await fetchBrands()
+  // mySwiper ref'i null olmadığından ve autoplay özelliğine sahip olduğundan emin olun
+  nextTick(() => {
+    if (mySwiper.value && mySwiper.value.autoplay) {
+      mySwiper.value.autoplay.start()
+    }
+  })
+})
 </script>
 
 <template>
   <div class="w-full mt-20">
-    <div class="w-full flex justify-between">
-      <h4 class="text-2xl font-bold">
-        <i class="ri-shield-check-fill text-green-500"></i>
-        Offical Store
-      </h4>
-      <h4 class="text-third-100">See All</h4>
-    </div>
     <div class="w-full flex p-4 gap-4 relative">
-      <div
-        class="absolute z-10 flex justify-center items-center top-[45%] -right-5 w-[50px] h-[50px] cursor-pointer bg-white gb-shadow rounded-full"
-        @click="mySwiper.slideNext()"
-      >
-        <i class="ri-arrow-right-s-line text-2xl"></i>
-      </div>
-      <div
-        class="absolute z-10 flex justify-center items-center top-[45%] -left-5 w-[50px] h-[50px] cursor-pointer bg-white gb-shadow rounded-full"
-        @click="mySwiper.slidePrev()"
-      >
-        <i class="ri-arrow-left-s-line text-2xl"></i>
-      </div>
       <swiper
         style="padding: 0 8px 10px 8px"
         class="w-full pb-2"
         :slides-per-view="'auto'"
         :space-between="10"
-        @swiper="onSwiper"
+        :loop="true"
+        :autoplay="{ delay: 3000, disableOnInteraction: false }"
+        @Swiper="onSwiper"
       >
         <swiper-slide v-for="brand in brands.data" :key="brand._id" style="width: auto" class="w-fit">
           <RouterLink :key="brand._id" :to="`/brands/${brand._id}`">
